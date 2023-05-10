2a1b9009961551a52f701e7cb76780ab89fe702c
AreebSiddiqui <areeb40@hotmail.com>
2023-05-10 17:32:47 +0500

    init commit

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const express = require('express');
-const app = express();
-app.get('/', (req,res)=>{
- res.send('hello! main!');
-});
-app.listen(3000);
+const express = require('express'); const app = express(); app.get('/', (req,res)=>{ res.sendStatus(200)});app.listen(3000);
